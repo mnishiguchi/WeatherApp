2d31e7bb1a67ddd4c323a7717b7c5343e5f5e8b1
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-06-08 08:43:04 -0400

    Replace hello text-view with recycler-view

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # WeatherApp
 
 This app is an exercise from [Kotlin for Android Developers by Antonio Leiva](https://leanpub.com/kotlin-for-android-developers)
+
+
+## References
+- [antoniolg/Kotlin-for-Android-Developers](https://github.com/antoniolg/Kotlin-for-Android-Developers)
+- [RecyclerView in Android: The basics](https://antonioleiva.com/recyclerview/)
--- app/src/main/java/com/mnishiguchi/weatherapp/MainActivity.kt
+++ app/src/main/java/com/mnishiguchi/weatherapp/MainActivity.kt
@@ -2,15 +2,29 @@ package com.mnishiguchi.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon - 75/55",
+            "Tue - 82/60",
+            "Wed - 88/66",
+            "Thu - 93/69",
+            "Fri - 94/72",
+            "Sat - 95/71",
+            "Sun - 96/73"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Powered by Anko
-        message.text = "Masatoshi"
+        // The good old findViewById()
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
